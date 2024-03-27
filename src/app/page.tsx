f8d57d95c304dc2e52b66ac1e87d4fc792ae3bad
Kelly <141122532+rmsoft-kelly@users.noreply.github.com>
2024-03-28 08:40:36 +0900

    reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-9xl">
+      <div>🎊</div>
+      <div>🥳</div>
+      <div>✨✨✨✨✨✨✨✨✨✨</div>
+    </main>
   );
 }
